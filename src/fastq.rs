da04eba7a9cbb824da9a44de7c5f9a907e9cb5fd
Joseph Guhlin <joseph.guhlin@gmail.com>
2022-01-24 13:18:49 +1300

    .

--- src/fastq.rs
+++ src/fastq.rs
@@ -1,6 +1,7 @@
 use bytelines::*;
-use crossbeam::channel::bounded;
+use crossbeam::channel::{bounded, Receiver, Sender};
 use crossbeam::thread;
+use flate2::read::MultiGzDecoder;
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use hashbrown::HashMap;
@@ -12,6 +13,8 @@ use twox_hash::xxh3::RandomHashBuilder64;
 use std::fs;
 use std::fs::File;
 use std::io::{BufReader, BufWriter, Read, Write};
+use std::path::PathBuf;
+use std::sync::Arc;
 // use std::thread;
 
 /* pub struct FastqEntry<'fq> {
@@ -199,77 +202,226 @@ impl FastqSplitter {
 
         assigned_barcodes
     }
+}
 
-    pub fn split_by_barcodes<R: Read + Send + Sync>(
-        &self,
-        reader: R,
-        suffix: String,
-        output_directory: String,
-        barcodes: Vec<(String, String, String, String, String)>,
-    ) {
-        let (sender, receiver) = bounded(8192);
+pub fn split_by_barcodes<R: Read + Send + Sync>(
+    reader: R,
+    suffix: String,
+    output_directory: String,
+    barcodes: Arc<Vec<(String, String, String, String, String)>>,
+    index_files: Option<(&PathBuf, &PathBuf)>,
+) {
+    let (sender, receiver) = bounded(8192);
 
-        let mut files = HashMap::new();
-        fs::create_dir_all(&output_directory).expect("Unable to create directory");
+    fs::create_dir_all(&output_directory).expect("Unable to create directory");
 
-        let mut assigned_barcodes: HashMap<String, String, RandomHashBuilder64> =
-        Default::default();
+    let mut assigned_barcodes: HashMap<String, String, RandomHashBuilder64> = Default::default();
 
-        for (_, _, id, _, _) in barcodes {
-            let file = BufWriter::new(GzEncoder::new(
-                File::create(format!("{}/{}_{}.fq.gz", output_directory, id, suffix)).unwrap(),
-                Compression::default(),
-            ));
-            files.insert(id.clone(), file);
-        }
+    thread::scope(|s| {
+        let mut index_receiver1 = None;
+        let mut index_receiver2 = None;
 
-        let mut assigned_barcodes: HashMap<String, String, RandomHashBuilder64> =
-            Default::default();
+        if let Some((idx1, idx2)) = index_files {
+            let (s1, ir1) = bounded(1);
+            let (s2, ir2) = bounded(1);
 
-        thread::scope(|s| {
-            let mut lines = BufReader::new(reader).byte_lines();
+            index_receiver1 = Some(ir1);
+            index_receiver2 = Some(ir2);
 
+            // Read the barcodes...
             s.spawn(move |_| {
+                let fh1 = File::open(idx1).expect("Unable to open file");
+                let fh1 = MultiGzDecoder::new(BufReader::new(fh1));
+                let fh2 = File::open(idx2).expect("Unable to open file");
+                let fh2 = MultiGzDecoder::new(BufReader::new(fh2));
+
+                let mut lines1 = BufReader::new(fh1).byte_lines();
+                let mut lines2 = BufReader::new(fh2).byte_lines();
+
                 loop {
                     let header;
-                    let id;
-        
-                    match lines.next() {
+
+                    match lines1.next() {
                         Some(Ok(line)) => {
                             header = from_utf8(line)
                                 .expect("FASTQ Header line is not valid UTF-8")
                                 .clone();
-                            let n = header.len();
-        
-                            id = header[n - 17..n].to_string().clone();
 
-                            let entry = 
-        
-                            if let Some(mut file) = files.get_mut(&id) {
-                                file.write_all(line).expect("Unable to write output file");
-                                writeln!(&mut file).expect("Unable to write output file");
-                                file.write_all(lines.next().unwrap().unwrap()).unwrap();
-                                writeln!(&mut file).expect("Unable to write output file");
-                                file.write_all(lines.next().unwrap().unwrap()).unwrap();
-                                writeln!(&mut file).expect("Unable to write output file");
-                                file.write_all(lines.next().unwrap().unwrap()).unwrap();
-                                writeln!(&mut file).expect("Unable to write output file");
-                            } else {
-                                println!("ID {} Not Found!", id);
-                            }
+                            let entry = (
+                                header.to_string().clone(),
+                                from_utf8(lines1.next().unwrap().unwrap())
+                                    .unwrap()
+                                    .to_string()
+                                    .clone(),
+                            );
+
+                            s1.send(Some(entry)).expect("Error sending Fastq Barcode");
+                            lines1.next();
+                            lines1.next();
                         }
                         _ => {
+                            s1.send(None).expect("Unable to send Done command");
+                            break;
+                        }
+                    };
+
+                    let header;
+
+                    match lines2.next() {
+                        Some(Ok(line)) => {
+                            header = from_utf8(line)
+                                .expect("FASTQ Header line is not valid UTF-8")
+                                .clone();
+
+                            let entry = (
+                                header.to_string().clone(),
+                                from_utf8(lines2.next().unwrap().unwrap())
+                                    .unwrap()
+                                    .to_string()
+                                    .clone(),
+                            );
+
+                            s2.send(Some(entry)).expect("Error sending Fastq Barcode");
+                            lines2.next();
+                            lines2.next();
+                        }
+                        _ => {
+                            s2.send(None).expect("Unable to send Done command");
                             break;
                         }
                     };
                 }
-
             });
+        }
 
+        s.spawn(move |_| {
+            let mut lines = BufReader::new(reader).byte_lines();
+
+            loop {
+                let header;
+                let id;
+
+                match lines.next() {
+                    Some(Ok(line)) => {
+                        header = from_utf8(line)
+                            .expect("FASTQ Header line is not valid UTF-8")
+                            .clone();
+
+                        if index_receiver1.is_some() {
+                            let i1 = index_receiver1.as_ref().unwrap().recv().expect("Error with barcode 1").expect("Error with barcode 1");
+                            let i2 = index_receiver2.as_ref().unwrap().recv().expect("Error with barcode 2").expect("Error with barcode 2");
+
+                            assert!(header == i1.0);
+                            assert!(header == i2.0);
+
+                            id = format!("{}+{}", i1.1, i2.1);
+
+                        } else {
+                            let n = header.len();
+                            id = header[n - 17..n].to_string().clone();
+                        }
+
+                        let entry = (
+                            id,
+                            header.to_string().clone(),
+                            from_utf8(lines.next().unwrap().unwrap())
+                                .unwrap()
+                                .to_string()
+                                .clone(),
+                            from_utf8(lines.next().unwrap().unwrap())
+                                .unwrap()
+                                .to_string()
+                                .clone(),
+                            from_utf8(lines.next().unwrap().unwrap())
+                                .unwrap()
+                                .to_string()
+                                .clone(),
+                        );
+
+                        sender.send(Some(entry)).expect("Error sending Fastq entry");
+                    }
+                    _ => {
+                        sender.send(None).expect("Unable to send Done command");
+                        break;
+                    }
+                };
+            }
         });
-        
-        
-    }
+
+        // END OF THREAD
+
+        let mut files: HashMap<String, Sender<Option<[String; 4]>>> = HashMap::new();
+
+        let mut ids = (*barcodes)
+            .iter()
+            .map(|x| x.2.clone())
+            .collect::<Vec<String>>();
+        ids.push("AMBIGUOUS".to_string());
+        ids.push("UNASSIGNED".to_string());
+
+        for id in ids.into_iter() {
+            let (send, r) = bounded(2048);
+            files.insert(id.clone(), send);
+
+            let output_directory = output_directory.clone();
+            let suffix = suffix.clone();
+
+            s.spawn(move |_| {
+                let mut file = BufWriter::new(GzEncoder::new(
+                    File::create(format!("{}/{}_{}.fq.gz", output_directory, id, suffix)).unwrap(),
+                    Compression::fast(),
+                ));
+
+                while let Ok(Some(e1)) = r.recv() {
+                    for e in e1.iter() {
+                        file.write_all(e.as_bytes()).unwrap();
+                        writeln!(file).unwrap();
+                    }
+                }
+            });
+        }
+
+        while let Ok(Some((id, header, e1, e2, e3))) = receiver.recv() {
+            let x = match assigned_barcodes.get(&id) {
+                Some(x) => x,
+                None => {
+                    let read_barcodes: Vec<&str> = id.split("+").collect();
+                    let mut scores: HashMap<String, u32, RandomHashBuilder64> = Default::default();
+                    for (bc0, bc1, id, _r1_file, _r2_file) in barcodes.iter() {
+                        let dist1 = levenshtein(read_barcodes[0].as_bytes(), bc0.as_bytes());
+                        let dist2 = levenshtein(read_barcodes[1].as_bytes(), bc1.as_bytes());
+
+                        scores.insert(id.clone(), dist1 + dist2);
+                    }
+                    let mut scores_vec: Vec<(&String, &u32)> = scores.iter().collect();
+                    scores_vec.sort_by(|a, b| b.1.cmp(a.1));
+                    scores_vec.reverse();
+
+                    let min = scores_vec[0].1;
+                    let min_id = scores_vec[0].0;
+
+                    if min == scores_vec[1].1 && *min <= 4 {
+                        assigned_barcodes.insert(id.clone(), "AMBIGUOUS".to_string());
+                    } else if *min <= 4 {
+                        assigned_barcodes.insert(id.clone(), min_id.clone());
+                    } else {
+                        assigned_barcodes.insert(id.clone(), "UNASSIGNED".to_string());
+                    }
+
+                    assigned_barcodes.get(&id).unwrap()
+                }
+            };
+
+            let send = files.get_mut(x).unwrap();
+            send.send(Some([header, e1, e2, e3]))
+                .expect("Error sending Fastq entry");
+        }
+
+        for i in files.values_mut() {
+            i.send(None).expect("Unable to send Finish command");
+        }
+    })
+    .unwrap();
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+use std::ffi::OsStr;
+use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
+use std::path::Path;
+use std::sync::Arc;
 
-use clap::{App, Parser};
+use clap::{App, AppSettings, Parser, Subcommand};
+use crossbeam::channel::{bounded, Receiver, Sender};
+use crossbeam::thread;
 use flate2::read::MultiGzDecoder;
-use indicatif::ProgressBar;
 use indicatif::ProgressStyle;
+use indicatif::{MultiProgress, ProgressBar};
+use wax::{Glob, Pattern};
 
 mod fastq;
-use fastq::FastqSplitter;
+use fastq::*;
 
 #[derive(Parser)]
 #[clap(name = "deezmux")]
@@ -16,8 +23,13 @@ use fastq::FastqSplitter;
 #[clap(about = "Fast demultiplexing of Illumina files where the barcode IDs are in the header", long_about = None)]
 struct Cli {
     barcode_file: String,
-    //    #[clap(last = true)]
-    read_files: Vec<String>,
+    output_directory: String,
+    read_prefix: String,
+}
+
+enum Mode {
+    BarcodesInHeader,
+    BarcodesInSeparateFile,
 }
 
 fn parse_barcode_file(barcode_file: &str) -> Vec<(String, String, String, String, String)> {
@@ -31,11 +43,11 @@ fn parse_barcode_file(barcode_file: &str) -> Vec<(String, String, String, String
         let j: Vec<&str> = line.split(",").collect();
         let z: Vec<&str> = j[1].split("+").collect();
         barcodes.push((
-            z[0].to_string(),
-            z[1].to_string(),
-            j[0].to_string(),
-            j[2].to_string(),
-            j[3].to_string(),
+            z[0].to_string(), // Barcode 0
+            z[1].to_string(), // Barcode 1
+            j[0].to_string(), // Sample ID
+            j[2].to_string(), // Barcode 0 sequence
+            j[3].to_string(), // Barcode 1 sequence
         ));
     }
 
@@ -55,39 +67,136 @@ fn main() {
         None => panic!("No read files specified"),
     }; */
     let barcode_file = &args.barcode_file;
-    let files = &args.read_files;
 
     println!("Parsing barcode file: {}", barcode_file);
+    let barcodes = Arc::new(parse_barcode_file(barcode_file));
 
-    let barcodes = parse_barcode_file(barcode_file);
+    let output_directory = &args.output_directory;
+    let prefix = &args.read_prefix;
 
-    let fqs = FastqSplitter::new().with_barcodes(barcodes).with_mm(2, 2);
+    // Figure out what type of files we are dealing with
 
-    for (i, file) in files.iter().enumerate() {
-        let r = format!("r{}", i + 1);
-        println!("Processing {} as {}", file, r);
-        let file_pb = File::open(file).expect("Unable to open file");
-        let pb = ProgressBar::new(file_pb.metadata().unwrap().len());
-        pb.set_style(
-            ProgressStyle::default_bar()
-                .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {eta}"),
-        );
-        let barcodes = fqs.match_barcodes(MultiGzDecoder::new(BufReader::new(
-            pb.wrap_read(&file_pb),
-        )));
+    let prefix_directory = Path::new(prefix)
+        .parent()
+        .expect("Unable to determine parent directory of prefix path")
+        .canonicalize()
+        .expect("Unable to canonicalize prefix path");
+    let prefix_files = Path::new(prefix).file_name().unwrap().to_str().unwrap();
+    let glob = format!("{}*.gz", prefix_files).to_string();
 
-        let file_pb = File::open(file).expect("Unable to open file");
-        let pb = ProgressBar::new(file_pb.metadata().unwrap().len());
-        pb.set_style(
-            ProgressStyle::default_bar()
-                .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {eta}"),
-        );
+    let glob = Glob::new(&glob).expect("Unable to create glob");
+    let mut files = Vec::with_capacity(4);
 
-        fqs.split_by_barcodes(
-            MultiGzDecoder::new(BufReader::new(pb.wrap_read(&file_pb))),
-            r,
-            "output".to_string(),
-            barcodes,
-        );
+    for i in glob.walk(prefix_directory, 1) {
+        match i {
+            Ok(x) => {
+                files.push(x.into_path());
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
     }
+
+    let mode = match files.len() {
+        0 => panic!("No files found matching prefix provided"),
+        1 => {
+            panic!("Only one file found matching prefix. Expected paired reads")
+        }
+        2 => {
+            println!("Found two files matching prefix. Assuming paired reads and barcode found in header");
+            Mode::BarcodesInHeader
+        }
+        4 => {
+            println!(
+                "Found four files matching prefix. Assuming paired reads and barcode in I files"
+            );
+            Mode::BarcodesInSeparateFile
+        }
+        _ => {
+            panic!("Found unusual number of files matching prefix. Expected paired reads")
+        }
+    };
+
+    let index_files = match mode {
+        Mode::BarcodesInHeader => None,
+        Mode::BarcodesInSeparateFile => Some((
+            files
+                .iter()
+                .find(|&x| x.file_name().unwrap().to_str().unwrap().contains("_I1"))
+                .unwrap(),
+            files
+                .iter()
+                .find(|&x| x.file_name().unwrap().to_str().unwrap().contains("_I2"))
+                .unwrap(),
+        )),
+    };
+
+    let files = [
+        files
+            .iter()
+            .find(|&x| x.file_name().unwrap().to_str().unwrap().contains("_R1"))
+            .unwrap(),
+        files
+            .iter()
+            .find(|&x| x.file_name().unwrap().to_str().unwrap().contains("_R2"))
+            .unwrap(),
+    ];
+
+    // let fqs = FastqSplitter::new().with_mm(2, 2);
+
+    thread::scope(|s| {
+        let m = MultiProgress::new();
+        let mut t = Vec::new();
+
+        for (i, file) in files.iter().enumerate() {
+            let r = format!("r{}", i + 1);
+            // println!("Processing {} as {}", file, r);
+
+            /*
+            let file_pb = File::open(file).expect("Unable to open file");
+            let pb = ProgressBar::new(file_pb.metadata().unwrap().len());
+            pb.set_style(
+                ProgressStyle::default_bar()
+                    .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {eta}"),
+            );
+            let barcodes =
+                fqs.match_barcodes(MultiGzDecoder::new(BufReader::new(pb.wrap_read(&file_pb))));
+
+            */
+
+            let file_pb = File::open(file).expect("Unable to open file");
+            let pb = ProgressBar::new(file_pb.metadata().unwrap().len());
+            pb.set_style(
+                ProgressStyle::default_bar()
+                    .template("{spinner:.blue}▕{bar:.green}▏{bytes:>4}/{total_bytes:4} {eta}")
+                    // .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {eta}"),
+                    .progress_chars("█▇▆▅▄▃▂▁  "),
+            );
+
+            let file_fh = pb.wrap_read(file_pb);
+
+            let _pb = m.add(pb);
+
+            let barcodes = Arc::clone(&barcodes);
+
+            let handle = s.spawn(move |_| {
+                split_by_barcodes(
+                    MultiGzDecoder::new(BufReader::new(file_fh)),
+                    r,
+                    "output".to_string(),
+                    barcodes,
+                    index_files,
+                );
+            });
+            t.push(handle);
+        }
+
+        m.join().unwrap();
+
+        for i in t {
+            i.join().unwrap();
+        }
+    })
+    .expect("Unable to properly scope");
 }
